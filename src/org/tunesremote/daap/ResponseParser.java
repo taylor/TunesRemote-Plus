273a3ec51d777c988fd76c3143dea6a301b5378c
Emil A. Lefkof III <mellowaredev@gmail.com>
2011-03-25 11:52:04 +0000

    Release 2.3.1 fixes issues #10 and #38

--- src/org/tunesremote/daap/ResponseParser.java
+++ src/org/tunesremote/daap/ResponseParser.java
@@ -42,13 +42,11 @@ import android.util.Log;
 public class ResponseParser {
 
    public final static String TAG = ResponseParser.class.toString();
-   public final static Pattern BRANCHES = Pattern
-            .compile("(cmst|mlog|agal|mlcl|mshl|mlit|abro|abar|apso|caci|avdb|cmgt|aply|adb|casp|mdcl)");
+   public final static Pattern BRANCHES = Pattern.compile("(cmst|mlog|agal|mlcl|mshl|mlit|abro|abar|apso|caci|avdb|cmgt|aply|adbs|casp|mdcl)");
    public final static Pattern STRINGS = Pattern.compile("(minm|cann|cana|cang|canl|asaa|asal|asar)");
    public final static Pattern RAWS = Pattern.compile("(canp)");
 
-   public static int performSearch(byte[] raw, TagListener listener, Pattern listenFor, boolean haltmlit)
-            throws IOException {
+   public static int performSearch(byte[] raw, TagListener listener, Pattern listenFor, boolean haltmlit) throws IOException {
       Log.d(TAG, "ResponseParser performSearch...");
       final DataInputStream stream = new DataInputStream(new ByteArrayInputStream(raw));
       final int hits = ResponseParser.search(stream, listener, listenFor, stream.available(), haltmlit);
@@ -70,8 +68,7 @@ public class ResponseParser {
       return ResponseParser.parse(stream, null, null, stream.available());
    }
 
-   private static int search(DataInputStream raw, TagListener listener, Pattern listenFor, int handle, boolean haltmlit)
-            throws IOException {
+   private static int search(DataInputStream raw, TagListener listener, Pattern listenFor, int handle, boolean haltmlit) throws IOException {
       int progress = 0;
       int hits = 0;
 
@@ -109,8 +106,7 @@ public class ResponseParser {
       return hits;
    }
 
-   private static Response parse(DataInputStream raw, TagListener listener, Pattern listenFor, int handle)
-            throws IOException {
+   private static Response parse(DataInputStream raw, TagListener listener, Pattern listenFor, int handle) throws IOException {
       final Response resp = new Response();
       int progress = 0;
 
